d53cc8e7948bc7002ad4054a1a8fece163deb205
Alex Recker <alex@reckerfamily.com>
2019-07-28 09:23:48 -0500

    blog publish scrapt

--- roles/base/files/blog-publish.sh
+++ roles/base/files/blog-publish.sh
@@ -5,4 +5,4 @@ set -e
 DIR="$HOME/public/www.alexrecker.com"
 BUCKET="blog-bucket-1edd2xs506ohe"
 
-aws s3 sync --delete "$DIR/" "s3://$BUCKET/"
+aws-personal.sh aws s3 sync --delete "$DIR/" "s3://$BUCKET/"
